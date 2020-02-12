4460c59a62c3b745e1401f936d347d7224ad1582
Eltayeb Abdelrahman <52611456+elt-abdel@users.noreply.github.com>
2020-02-12 18:27:51 -0500

    javascript

---
+++ public/script.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("submit");
+button.onclick = submission;
+
+function submission() {
+    window.alert("Thanks for the comment!")
+  console.log("Thanks for the comment!")
+}
+
+
